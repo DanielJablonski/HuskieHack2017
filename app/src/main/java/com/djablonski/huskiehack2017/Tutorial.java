14de591dc4f9e675041943503a4af9509e0ad655
jholguin1 <jholguin1@cps.edu>
2017-11-04 16:51:00 -0500

    Activity fixes

--- app/src/main/java/com/djablonski/huskiehack2017/Tutorial.java
+++ app/src/main/java/com/djablonski/huskiehack2017/Tutorial.java
@@ -1,8 +1,19 @@
 package com.djablonski.huskiehack2017;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Jacob on 11/4/2017.
  */
 
-public class Tutorial {
+public class Tutorial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutorial_activity);
+
+        //this is a comment
+    }
 }
