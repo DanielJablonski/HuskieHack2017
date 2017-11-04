c0d46583901ed31a65bf2904bb82f4af5e5d8ceb
jholguin1 <jholguin1@cps.edu>
2017-11-04 16:59:29 -0500

    Added Questionnaire Activty

---
+++ app/src/main/java/com/djablonski/huskiehack2017/QuestionnaireActivty.java
@@ -0,0 +1,19 @@
+package com.djablonski.huskiehack2017;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jacob on 11/4/2017.
+ */
+
+public class QuestionnaireActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.questionnaire_activty);
+
+        //this is a comment
+    }
+}
