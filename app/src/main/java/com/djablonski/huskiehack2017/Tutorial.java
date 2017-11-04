578edee431add6b40080baedfbfdf1cdeee54e4e
jholguin1 <jholguin1@cps.edu>
2017-11-04 16:49:08 -0500

    Adding Screens

---
+++ app/src/main/java/com/djablonski/huskiehack2017/Tutorial.java
@@ -0,0 +1,8 @@
+package com.djablonski.huskiehack2017;
+
+/**
+ * Created by Jacob on 11/4/2017.
+ */
+
+public class Tutorial {
+}
---
+++ app/src/main/java/com/djablonski/huskiehack2017/dataScreen.java
@@ -0,0 +1,19 @@
+package com.djablonski.huskiehack2017;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jacob on 11/4/2017.
+ */
+
+public class dataScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_activity);
+
+        //this is a comment
+    }
+}
