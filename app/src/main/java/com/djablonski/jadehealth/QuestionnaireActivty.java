6a6cb05122b7cc0c8c667fed74517c6da87eafd3
jholguin1 <jholguin1@cps.edu>
2017-11-04 19:19:12 -0500

    Changed layout of questionnaire and data activties (WIP)

--- app/src/main/java/com/djablonski/jadehealth/QuestionnaireActivty.java
+++ app/src/main/java/com/djablonski/jadehealth/QuestionnaireActivty.java
@@ -2,6 +2,9 @@ package com.djablonski.jadehealth;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Jacob on 11/4/2017.
@@ -9,11 +12,25 @@ import android.support.v7.app.AppCompatActivity;
 
 public class QuestionnaireActivty extends AppCompatActivity {
 
+    private TextView title;
+    private TextView sex;
+    private TextView age;
+    private TextView height;
+    private TextView weight;
+    private CheckBox femaleBox;
+    private CheckBox maleBox;
+    private EditText ageInfo;
+    private EditText heightInfo;
+    private EditText weightInfo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.questionnaire_activty);
 
-        //this is a comment
+        title = findViewById(R.id.textView);
+        sex = findViewById(R.id.textView1);
+
+
     }
 }
